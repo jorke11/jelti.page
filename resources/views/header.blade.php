a51b1728b2ef507c52d7f4a5d4a639afe7c7ebcf
jorge pinedo <jpinedom@hotmail.com>
2018-08-22 15:50:06 -0500

    improve web page

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -133,7 +133,7 @@
                         </li>-->
         </ul>
 
-        <ul class="navbar-nav mr-auto text-center" style="padding-left: 20%;padding-top: 15px">
+        <ul class="navbar-nav mr-auto text-center" style="padding-left: 15%;padding-top: 15px">
             <li class="nav-item">
                 <a href="{{url("/")}}"><img alt="Brand" src="{{asset('images/page/logosuperf.svg') }}" width="60%" /></a>
             </li>
