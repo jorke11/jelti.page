ce5f98922d03233ade928ac36afd1c2be60aef9e
jorge pinedo <jpinedom@hotmail.com>
2018-08-28 12:51:46 -0500

    add new links

--- resources/views/listproducts.blade.php
+++ resources/views/listproducts.blade.php
@@ -126,7 +126,7 @@
 <section>
     <div class="container-fluid"  id="content-image" style="padding-left: 0; padding-right: 0;padding-bottom: 5%;position:relative;top:-200px;min-height: 100px">
         <div class="row center-block" style="margin-right: 0;padding-top:1%">
-            <div class="col-lg-2 offset-1 ">
+            <div class="col-lg-10 offset-1 ">
                 <span><?php echo $breadcrumbs ?></span>
             </div>
         </div>
