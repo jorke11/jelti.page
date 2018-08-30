c23f04b12f03c40bc6a3aeaddaa5b6076cc85494
jorge pinedo <jpinedom@hotmail.com>
2018-08-30 18:46:22 -0500

    add

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -288,6 +288,7 @@ class PageController extends Controller {
                 $end = date("Y-m-d");
                 $field = '';
                 $join = '';
+                $group= '';
                 $sql = "
             SELECT p.id,p.title as product,sup.business as supplier, 
             sum(CASE WHEN d.real_quantity IS NULL THEN 0 ELSE d.real_quantity end * CASE WHEN d.packaging=0 THEN 1 WHEN d.packaging IS NULL THEN 1 ELSE d.packaging END) quantity, 
