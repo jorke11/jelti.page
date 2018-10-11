436def68a0b3d79a2e1c24dedf26d1e8f7d489cf
jorge pinedo <jpinedom@hotmail.com>
2018-10-10 19:38:59 -0500

    change method create url link to activate account

--- resources/views/Notifications/activation.blade.php
+++ resources/views/Notifications/activation.blade.php
@@ -6,7 +6,7 @@
         <td>Te damos la mas cordial bienvenida a nuestra plataforma de Compras Superfuds</td>
     </tr>
     <tr>
-        <td>Para Activar tu cuenta click en</td><td><a href="{{$link}}">{{$link}}</td>
+        <td>Para Activar tu cuenta click en</td><td><a href="{{url("user/activation",$link)}}">{{url("user/activation",$link)}}</a></td>
     </tr>
     <tr>
         <td>Usuario</td><td>{{$email}}</td>
